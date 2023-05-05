5227fa94875076e2a7b4ea8f0cb77c90887d3a2d
rhyttahkogi@gmail.com <rkogi2022>
2023-05-05 11:56:51 +0300

    adding a datepicker to datefields

--- support/forms.py
+++ support/forms.py
@@ -10,19 +10,20 @@ class DateInput(forms.DateInput):
     input_type = 'date'
 
 class AddNewClient(forms.ModelForm):
-    
+    logdate=forms.DateTimeField(label='Date',widget=DateInput)
+    completiondate=forms.DateTimeField(label='Date',widget=DateInput)
     class Meta:
         model=support
         fields=('__all__')
 
 class AddCourtesyCall(forms.ModelForm):
-    
+    logdate=forms.DateTimeField(label='Date',widget=DateInput)
     class Meta:
         model=courtesy
         fields=('__all__')
 
 class AddBimonthlyCall(forms.ModelForm):
-    
+    logdate=forms.DateTimeField(label='Date',widget=DateInput)
     class Meta:
         model=bimonthly
         fields=('__all__')
